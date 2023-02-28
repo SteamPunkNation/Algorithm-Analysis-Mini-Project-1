47f18694addef2b452e2c0f2cefc27639d727a5c
SteamPunkNation <steampunknation.ad@gmail.com>
2023-02-28 09:54:24 -0500

    Basic start of graph mapping and neighborhood plotting

---
+++ Project.m
@@ -0,0 +1,8 @@
+plot(1000,1000);
+xlabel('x');
+ylabel('y');
+title('Population Concentration');
+!Comment here
+neighborhoodX=rand(1,5)*1000;
+neighborhoodY=rand(1,5)*1000;
+neighboorhoodCenter=scatter(neighborhoodX,neighborhoodY);
